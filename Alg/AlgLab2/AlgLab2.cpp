c53bd6cb0eca99d8c3de890306b7b96132b4c908
t3moxa <vyachistyy@yandex.ru>
2023-09-25 12:05:36 +0700

    Update AlgLab2.cpp

--- Alg/AlgLab2/AlgLab2.cpp
+++ Alg/AlgLab2/AlgLab2.cpp
@@ -4,73 +4,79 @@ using namespace std;
 class HashTable
 {
 public:
-    int q, r, s, CompareAmount;
-    const char** Table;
-    // const char * const * const Table = {};
+    int q, r, s;
+    char** Table;
     HashTable()
     {
-        CompareAmount = 0;
         cout << "Size of the table:\n";
         cin >> q;
-        Table = new const char* [q];
+        Table = new char* [q];
         for (int i = 0; i < q; i++)
             Table[i] = NULL;
         cout << "r and s for second function:\n";
         cin >> r >> s;
     }
-    static int HashFunc(const char* S, int L, int q)
+    static int Collision(int f1, HashTable t)
+    {
+        int s = 0;
+        while ((f1 + s) != NULL)
+            s += HashFunc2(f1, t);
+        return s;
+    }
+    static int HashFunc(char* S, int L, HashTable t)
     {
         int f = 0;
         for (int i = 0; i < L; i++)
         {
             f += S[i];
-            f = f % q * 31;
+            f = f % t.q * 31;
         }
-        f = f % q;
+        f = f % t.q;
         return f;
     }
     static int HashFunc2(int f1, HashTable t)
     {
         return f1 % t.r + t.s;
     }
-    static void AddToTable(const char* S, int L, HashTable t)
+    static void AddToTable(char* S, int L, HashTable t)
     {
-        int f1 = HashFunc(S, L, t.q);
+        int f1 = HashFunc(S, L, t);
         int f2 = HashFunc2(f1, t);
-        if (SearchString(S, L, t)==-1)
+        if (t.Table[f1] == NULL)
         {
-            
+            cout << "fit" << endl;
         }
-        while (t.Table[f1] != NULL)
-            f1 = (f1 + f2) % t.q;
-        t.Table[f1] = S;
-        //cout << S << " added" << endl;
-    }
-    void ShowTable() const
-    {
-        for (int i = 0; i < q; i++)
+        else
         {
-            if (this->Table[i] == NULL)
+            cout << "not fit" << endl;
+            while (t.Table[f1] != NULL)
+                f1 = (f1 + f2) % t.q;
+        }
+        t.Table[f1] = S;
+        cout << S << " added" << endl;
+    }
+    static void ShowTable(HashTable t)
+    {
+        for (int i = 0; i < t.q; i++)
+        {
+            if (t.Table[i] == NULL)
                 cout << "-" << endl;
             else
-                cout << Table[i] << endl;
+                cout << t.Table[i] << endl;
         }
     }
-    static int SearchString(const char* S, int L, HashTable t)
+    static int SearchString(char* S, int L, HashTable t)
     {
-        int f1 = HashFunc(S, L, t.q);
+        int f1 = HashFunc(S, L, t);
         int f2 = HashFunc2(f1, t);
-        while ((t.Table[f1] != NULL) && (t.Table[f1] != S))
-        {
+        while (t.Table[f1] != NULL && t.Table[f1] != S)
             f1 = (f1 + f2) % t.q;
-            t.CompareAmount++;
-        }
         if (t.Table[f1] == S)
             return f1;
         else
             return -1;
     }
-    static void ShowString(int i, const HashTable t)
+    static void ShowString(int i, HashTable t)
     {
         cout << t.Table[i];
     }
@@ -78,7 +84,7 @@ public:
 
 char* RandString(int l)
 {
-    char* c = new char[l];
+    char* c = new char[l+1];
     for (int i = 0; i < l; i++)
     {
         c[i] = char('a' + rand() % ('z' - 'a'));
@@ -88,37 +94,22 @@ char* RandString(int l)
 }
 int main()
 {
-    cout << "sizeof(int) = " << sizeof(int) << " sizeof(HashTable) = " << sizeof(HashTable) << endl;
-    int l, i, amount, UniqueCount = 0;
+    int l, i, amount;
     HashTable aboba;
     cout << "Enter amount of strings:\n";
     cin >> amount;
     cout << "Enter size of the strings:\n";
     cin >> l;
     char **c = new char*[amount];
-    //HashTable::ShowTable(aboba);
+    HashTable::ShowTable(aboba);
     for (i = 0; i < amount; i++)
     {
-        c[i] = RandString(rand()%5);
+        c[i] = RandString(l);
         cout << c[i] << endl;
         if (HashTable::SearchString(c[i], l, aboba) == -1)
-        {
-            UniqueCount++;
             HashTable::AddToTable(c[i], l, aboba);
-        }
     }
-    cout << "UniqueCount = " << UniqueCount << endl;
-    const char *SearchedString = "foo";
-    //HashTable::ShowTable(aboba);
-    aboba.ShowTable();
-    //cout << endl << SearchedString << " " << endl << HashTable::SearchString(SearchedString, l, aboba) << endl;
-    if (HashTable::SearchString(SearchedString, l, aboba) != -1)
-    {
-        cout << "Found string: ";
-        HashTable::ShowString(HashTable::SearchString(SearchedString, l, aboba), aboba);
-        cout << endl;
-    }
-    else
-        cout << "String not found" << endl;
-    cout << aboba.CompareAmount;
+    cout << endl;
+    HashTable::ShowTable(aboba);
+    HashTable::ShowString(HashTable::SearchString(c[2], l, aboba), aboba);
 }
