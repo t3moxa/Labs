2ad665a69a3d3ea921afb477c76779af98ba5947
Вячистый Тимофей Дмитриевич <133843440+t3moxa@users.noreply.github.com>
2023-10-17 13:56:37 +0700

    Create Lab1.cs

---
+++ OOAP/Lab1.cs
@@ -0,0 +1,30 @@
+public abstract class Character
+{
+  shared name String;
+  shared strength int;
+  shared dexterity int;
+  shared endurance int;
+  shared masteryOfFire int;
+  shared masteryOfWater int;
+  shared masteryOfAir int;
+  shared masteryOfEarth int;
+  shared light int;
+  shared dark int;
+  public calculateStats()
+  {
+    
+  }
+  public equip(entity basicDevEntity)
+  {
+    
+  }
+}
+public class Player: Character
+{
+public saveGame()
+{
+}
+}
+public class Enemy: Character
+{
+}
